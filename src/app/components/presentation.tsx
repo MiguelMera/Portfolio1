eb6f52ede4a85f233c8fbdca2d0d783b4e4ce5c5
MiguelMera <tiltedlobster@gmail.com>
2024-10-14 23:23:07 -0500

    Update presentation.tsx
    updated responsive images

--- src/app/components/presentation.tsx
+++ src/app/components/presentation.tsx
@@ -3,20 +3,22 @@ import Image from 'next/image';
 const Presentation = () => {
   return (
     <div className="min-h-screen relative w-full flex justify-center items-center flex-col shadow-lg max-w-full overflow-x-hidden fade-in" id="presentation">
+      
       <div className="relative flex justify-center items-center">
         <Image
           src="/Rectangle3.png"
-          alt="image"
+          alt="background image"
           width={600}
           height={600}
           priority={true}
-          className="w-full md:w-full shado animate-fade-in-up"
+          className="w-full md:w-full animate-fade-in-up"
         />
 
-        <div className="absolute transform flex top-3 animate-fade-in-up">
+        
+        <div className="absolute top-14 transform flex justify-center animate-fade-in-up">
           <Image
             src="/yo.png"
-            alt="image"
+            alt="profile image"
             width={150}
             height={150}
             className="rounded-full"
@@ -24,7 +26,8 @@ const Presentation = () => {
         </div>
       </div>
 
-      <div className="relative text-center -mt-72 z-20 w-full md:px-4 px-20 max-w-md md:max-w-lg mx-auto animate-fade-in-up">
+      
+      <div className="relative text-center -mt-28 md:-mt-64 z-20 w-full md:px-4 px-20 max-w-md md:max-w-lg mx-auto animate-fade-in-up">
         <h1 className="text-sm md:text-2xl font-bold green-animate pb-4">Hello! I am Miguel</h1>
         <p className="text-lg md:text-base leading-relaxed text-white">
          {` I'm a Junior Frontend Developer with 2 years of hands-on experience in web development, specializing in React and Next.js. Throughout my journey, I have built a solid foundation in creating responsive, user-friendly interfaces and modern web applications. My passion for coding, along with my continuous drive to learn, allows me to deliver clean, efficient, and scalable code.`}
@@ -39,4 +42,3 @@ const Presentation = () => {
 };
 
 export default Presentation;
-
